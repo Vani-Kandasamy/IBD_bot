18f947345c1cf1ad67788555ab9b1583affba213
26keyam <115429745+26keyam@users.noreply.github.com>
2025-01-07 02:11:45 +0000

    Adding some files

--- home.py
+++ home.py
@@ -4,6 +4,6 @@ import streamlit as st
 IMAGE_ADDRESS = "https://aboutibs.org/wp-content/uploads/sites/13/Diet-and-IBS-768x512.jpg"
 
 # web app
-st.title("IBS Control Helper")
+st.title("IBD Control Helper")
 
-st.image(IMAGE_ADDRESS, caption = "IBS Nutrition Importance")
+st.image(IMAGE_ADDRESS, caption = "IBD Nutrition Importance")
