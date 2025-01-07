0dfc6ac33e4f8edf12e1661612a32d7bcdf5517b
26keyam <115429745+26keyam@users.noreply.github.com>
2025-01-07 02:09:59 +0000

    Building combined app

---
+++ home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+IMAGE_ADDRESS = "https://aboutibs.org/wp-content/uploads/sites/13/Diet-and-IBS-768x512.jpg"
+
+# web app
+st.title("IBS Control Helper")
+
+st.image(IMAGE_ADDRESS, caption = "IBS Nutrition Importance")
---
+++ pages/2_imagebot.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from PIL import Image
+from image_graph import graph_streamer
+
+IMAGE_ADDRESS = "https://upload.wikimedia.org/wikipedia/commons/1/1a/Irritable_bowel_syndrome.jpg"
+IMAGE_NAME = "uploaded_image.png"
+
+# title
+st.title("IBSee Guide")
+# image
+st.image(IMAGE_ADDRESS, caption = "IBS Disease Helper")
+
+# Upload image
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file:
+    # open the image
+    image = Image.open(uploaded_file)
+
+    # display the image
+    st.header("Uploaded Image")
+    st.image(image, caption='Uploaded Image.', use_container_width=True)
+
+    # save the image as a PNG file
+    image.save(IMAGE_NAME)
+
+    # analyse the image
+    with st.spinner("Generating Information......"):
+        get_chip_type = st.write_stream(graph_streamer(IMAGE_NAME))
+        st.toast('Information Generation Successful!', icon='✅')
+
+    if not get_chip_type:
+        st.error("Cannot Interpret the Image", icon = "❌")
+        st.stop()
