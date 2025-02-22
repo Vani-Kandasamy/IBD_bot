1a09436023449bd4780a7a3bd29283c2498e9bb4
Vani-Kandasamy <vani.kandasamy@pyxeda.ai>
2025-02-22 06:49:54 +0000

    updated

--- homefinal.py
+++ homefinal.py
@@ -3,7 +3,7 @@ import streamlit as st
 from datetime import datetime
 import authlib
 
-from streamlit_app import app_main
+
 
 from google_integration import get_google_cloud_credentials, get_user_details, update_user_document
 import pandas as pd 
@@ -15,43 +15,12 @@ st.title("IBD Control Helper")
 
 st.image(IMAGE_ADDRESS, caption = "IBD Nutrition Importance")
 
-def set_up_credentials():
-    if 'credentials' not in st.session_state:
-        jstr = st.secrets.get('GOOGLE_KEY')
-        credentials = get_google_cloud_credentials(jstr)
-        st.session_state['credentials']=credentials
-    return st.session_state['credentials']
-
-def main_code():
-    
-    creds=set_up_credentials()
-    # Documentation for each key
-    selected_keys = ['email', 'name']
-    # Extract the key-value pairs from the dictionary
-    selected_data = {key: st.experimental_user[key] for key in selected_keys}
-
-    # Convert the dictionary to a DataFrame
-    df = pd.DataFrame([selected_data])
-
-    
-    st.subheader("User Information", divider=True)
-    # Create the DataFrame
-   
-
-    # Display the DataFrame in Streamlit
-    st.dataframe(df, hide_index=True)
-
-    
-config_auth_needed=st.secrets.get("AUTH_NEEDED","True").lower()=="true"
-
-auth_needed=config_auth_needed and not st.experimental_user.is_logged_in
-
-if auth_needed:
+if not st.experimental_user.is_logged_in:
     if st.sidebar.button("Log in with Google", type="primary", icon=":material/login:"):
         st.login()
 else:
     # Display user name
-    main_code()
-    app_main()
+    st.html(f"Hello, <span style='color: orange; font-weight: bold;'>{st.experimental_user.name}</span>!")
+    
     if st.sidebar.button("Log out", type="secondary", icon=":material/logout:"):
         st.logout()
--- pages/1_chatbot.py
+++ pages/1_chatbot.py
@@ -2,6 +2,9 @@ from openai import OpenAI
 import streamlit as st
 from text_graph import graph_streamer
 from langchain_core.messages import AIMessage, HumanMessage
+from google_integration import get_google_cloud_credentials, get_user_details, update_user_document
+import pandas as pd 
+
 
 IMAGE_ADDRESS = "https://upload.wikimedia.org/wikipedia/commons/1/1a/Irritable_bowel_syndrome.jpg"
 
@@ -42,3 +45,29 @@ if prompt := st.chat_input("What is up?"):
         print("Message List", message_list)
         response = st.write_stream(graph_streamer(message_list))
     st.session_state.messages.append({"role": "assistant", "content": response})
+
+def set_up_credentials():
+    if 'credentials' not in st.session_state:
+        jstr = st.secrets.get('GOOGLE_KEY')
+        credentials = get_google_cloud_credentials(jstr)
+        st.session_state['credentials']=credentials
+    return st.session_state['credentials']
+
+def main_code():
+    
+    creds=set_up_credentials()
+    # Documentation for each key
+    selected_keys = ['email', 'name']
+    # Extract the key-value pairs from the dictionary
+    selected_data = {key: st.experimental_user[key] for key in selected_keys}
+
+    # Convert the dictionary to a DataFrame
+    df = pd.DataFrame([selected_data])
+
+    
+    st.subheader("User Information", divider=True)
+    # Create the DataFrame
+   
+
+    # Display the DataFrame in Streamlit
+    st.dataframe(df, hide_index=True)
