54a01312993d27a9f9c5664b8ff5b0945fe76e29
Vani-Kandasamy <vani.kandasamy@pyxeda.ai>
2025-02-26 09:13:00 +0000

    error

--- Home.py
+++ Home.py
@@ -63,8 +63,8 @@ def main():
 
     if 'user_info' not in st.session_state:
         st.warning("Please log in to access the services.")
-        #st.markdown(f"[Login with Auth0]({login_url})")   
-        st.sidebar.button(f"[Login with Auth0]({login_url})")
+        st.markdown(f"[Login with Auth0]({login_url})")   
+        #st.sidebar.button(f"[Login with Auth0]({login_url})")
         st.login()
 
         if 'code' in st.experimental_get_query_params():
